6ede0961b8b50e2b6f49adcb623782931f831d72
konohanasakuya <saku.rory.a@gmail.com>
2023-01-17 17:33:17 +0900

    roomCreate

--- animal_apartment_2/Assets/script/moveScene.cs
+++ animal_apartment_2/Assets/script/moveScene.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class moveScene : MonoBehaviour
 {
+    public GameObject load;
+    public GameObject score;
+    public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
-        
+        load.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnMouseDown()
     {
-        
+        Debug.Log("move");
+        StartCoroutine("move");
+        //SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator move()
+    {
+        if (score!=null){
+        score.SetActive(false);
+        }
+        load.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(sceneName);
     }
 }
--- animal_apartment_2/Assets/script/picControl.cs
+++ animal_apartment_2/Assets/script/picControl.cs
@@ -8,14 +8,13 @@ public class picControl : MonoBehaviour
 {
     public GameObject loading; 
     public GameObject score; 
-    public SpriteRenderer picControl;
+    public SpriteRenderer control;
     public Sprite pointOn;
     public Sprite pointOff;
-    public string sceneName; //移動したいシーン名をここに入れる
     // Start is called before the first frame update
     void Start()
     {
-        picControl = gameObject.GetComponent<SpriteRenderer>();
+        control = gameObject.GetComponent<SpriteRenderer>();
         loading.SetActive(false);
         if (score!=null){
         score.SetActive(true);
@@ -31,12 +30,12 @@ public class picControl : MonoBehaviour
     //カーソルが触れているときに実行
     public void OnMouseOver()
     {
-        pointer.sprite = pointOn;
+        control.sprite = pointOn;
     }
 
     //カーソルが離れているときに実行
     public void OnMouseExit()
     {
-        pointer.sprite = pointOff;
+        control.sprite = pointOff;
     }
 }
